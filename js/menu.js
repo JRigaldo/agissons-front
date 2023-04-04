5c74c82e630bf01902d61b1515b98684218e3940
IDRigaldo <jeremy.rigaldo@idmobile.ch>
2023-04-04 12:59:52 +0200

    refactor aside menu N style aside col

--- js/menu.js
+++ js/menu.js
@@ -4,17 +4,17 @@ jQuery(function ($) {
             $("#header-icon-links").click(function (e) {
                 e.preventDefault()
                 $("body").addClass("with-sidebar-links")
-                $("html").css('overflow-y', 'hidden');
+                $("body").css({'overflow-y': 'hidden', 'min-height': '100%'});
             })
             $("#exit-menu-links").click(function (e) {
                 e.preventDefault()
                 $("body").removeClass("with-sidebar-links")
-                $("html").css('overflow-y', 'scroll');
+                $("body").css({'overflow-y': 'scroll', 'min-height': 'auto'});
             })
             $("#site-cover").click(function (e) {
                 e.preventDefault()
                 $("body").removeClass("with-sidebar-links")
-                $("html").css('overflow-y', 'scroll');
+                $("body").css({'overflow-y': 'scroll', 'min-height': 'auto'});
             })
         }
         responsive()
