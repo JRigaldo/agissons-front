67827eea4d033ce655ce078c86f617593bc79c9e
IDRigaldo <jeremy.rigaldo@idmobile.ch>
2023-04-27 23:05:00 +0200

    add text to political-vision

--- js/accordion.js
+++ js/accordion.js
@@ -1,12 +1,16 @@
 jQuery(function ($) {
     $(document).ready(function () {
         function accordion() {
-            $("#header-icon-links").click(function (e) {
-                e.preventDefault()
-                $("body").addClass("with-sidebar-links")
-                $("body").css({ 'overflow-y': 'hidden', 'min-height': '100%' });
+            $(".accordion .accordion--title").click(function (e) {
+                e.preventDefault();
+                $(this).parent().toggleClass('accordion-active');
+
             });
+            $(".sub-accordion .accordion--subtitle").click(function (e){
+                e.preventDefault();
+                $(this).parent().toggleClass('sub-accordion-active');
+            })
         }
-        responsive()
+        accordion()
     })
 })
